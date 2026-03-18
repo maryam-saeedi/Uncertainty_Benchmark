7b106c816448e2ccfdb51e1bf4c3b597f38974a5
maryam-saeedi <m.saeedifard@aut.ac.ir>
2026-03-18 10:54:58 +0100

    add deep models

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,8 @@
 from .model import Model
 from .model_factory import ModelFactory, register_model
 from .mlp import MLP
+from .densenet import DenseNet
+from .efficientnet import EfficientNet
 
-__all__ = ['Model', 'ModelFactory', 'register_model', 'MLP']
+__all__ = ['Model', 'ModelFactory', 'register_model', 'MLP', 'DenseNet', 'EfficientNet']
 
