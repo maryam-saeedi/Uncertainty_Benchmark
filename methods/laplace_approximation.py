62624bee5202e92f771f2f8f68ba640a262240c9
maryam-saeedi <m.saeedifard@aut.ac.ir>
2026-03-18 10:43:39 +0100

    fix Laplace Approximation

--- methods/laplace_approximation.py
+++ methods/laplace_approximation.py
@@ -1,5 +1,6 @@
 import os
 
+from methods import register_method
 from methods.method import Method
 from laplace import Laplace
 import torch
@@ -7,9 +8,8 @@ import numpy as np
 from tqdm import tqdm
 import pickle
 
-class MLP:
-    pass
 
+@register_method("laplace_approximation")
 class LaplaceApproximation(Method):
     def __init__(self, config):
         super(LaplaceApproximation, self).__init__(config)
@@ -21,13 +21,21 @@ class LaplaceApproximation(Method):
                                self.config.method.subset_of_weights,
                                self.config.method.hessian_structure)
 
-            self.laplace.load_state_dict(torch.load(os.path.join(self.config.method.output.path, 'model', 'laplace.pt'), map_location='cpu'))
+            model_name =  'model.pt'
+            if 'pretrained' in kwargs:
+                model_name = os.path.basename(kwargs['pretrained'])
+            self.laplace.model.eval()
+            self.laplace.load_state_dict(torch.load(os.path.join(self.config.output.path, self.config.method.name, 'model', model_name), map_location=self.device))
             return
-        self.train_method(kwargs['train_dl'], kwargs['valid_dl'])
-        output_save_dir = os.path.join(self.config.output.path, 'model')
+        self.train_method(kwargs['train_loader'], kwargs['valid_loader'])
+        output_save_dir = os.path.join(self.config.output.path, self.config.method.name, 'model')
         os.makedirs(output_save_dir, exist_ok=True)
-        with open(os.path.join(output_save_dir,'laplace.pkl'), 'wb') as f:
-            pickle.dump(self.laplace, f)
+        model = self.train_uncertainty_method(kwargs['train_loader'], kwargs['valid_loader'])
+        if 'model_name' in kwargs:
+            model_name = kwargs['model_name']
+        else:
+            model_name = f'model.pt'
+        torch.save(self.laplace.state_dict(), os.path.join(output_save_dir, model_name))
 
     def train_method(self, train_loader, val_loader):
 
@@ -43,33 +51,21 @@ class LaplaceApproximation(Method):
             val_loader=val_loader
         )
 
-    def measure_uncertainty(self, loader):
+    def inference(self, loader):
         self.model.eval()
         self.laplace.model.eval()
         predictions = []
+        labels = []
         for x_test, y_test in tqdm(loader):
             try:
                 # User-specified predictive approx.Laplace Redux – Effortless Bayesian Deep Learning
-                pred = self.laplace.predictive_samples(x_test.to(self.device), n_samples=1000)
-                pred = pred.squeeze(1).unsqueeze(0)
-                predictions.append(pred.cpu())
+                pred = self.laplace.predictive_samples(x_test.to(self.device), n_samples=self.config.method.num_samples)
+                predictions.append(pred)
+                labels.append(y_test.to(self.device))
             except Exception as e:
                 print(e)
                 # pass
 
-        predictions = torch.cat(predictions, dim=0)
-        aleatoric_uncertainty = -torch.mean(torch.sum(predictions * torch.log(predictions), dim=2), dim=1)
-
-        p_mean = torch.mean(predictions, dim=1)
-        total_uncertainty = -torch.sum(p_mean * torch.log(p_mean), dim=1)
-
-        # Mutual information (BALD) -> epistemic uncertainty
-        epistemic_uncertainty = total_uncertainty - aleatoric_uncertainty
-
-
-        return {
-            "total_uncertainty": total_uncertainty.detach().cpu().numpy(),
-            "aleatoric_uncertainty": aleatoric_uncertainty.detach().cpu().numpy(),
-            "epistemic_uncertainty": epistemic_uncertainty.detach().cpu().numpy(),
-            "out_of_distribution": 0,
-        }
+        predictions = torch.cat(predictions, dim=1)
+        labels = torch.cat(labels, dim=0)
+        return predictions, labels
